2de1e9f6ef9e3306694fdf7346cde27b444dcd85
Bruno Farias <bruno.cabral88@hotmail.com>
2017-09-03 22:47:01 -0300

    Initial working implementation

--- AlcoolOuGasolina/ViewController.swift
+++ AlcoolOuGasolina/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var txtPrecoAlcool: UITextField!
+    @IBOutlet weak var txtPrecoGasolina: UITextField!
+    @IBOutlet weak var txtResultado: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +27,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: Actions
+    
+    @IBAction func calcular(_ sender: UIButton) {
+        
+        if let precoAlcool = txtPrecoAlcool.text {
+            if let precoGasolina = txtPrecoGasolina.text {
+                let resultado = validarCampo(precoAlcool: precoAlcool, precoGasolina: precoGasolina)
+                if resultado {
+                    calcularMelhorPreco(precoAlcool: precoAlcool, precoGasolina: precoGasolina)
+                } else {
+                    txtResultado.text = "Digite os valores para calcualar"
+                }
+            }
+        }
+    }
+    
+    func calcularMelhorPreco(precoAlcool: String, precoGasolina: String) {
+        if let valorAlcool = Double(precoAlcool) {
+            if let valorGasolina = Double(precoGasolina) {
+                if valorGasolina * 0.70 > valorAlcool {
+                    txtResultado.text = "Melhor colocar alcool!"
+                } else {
+                    txtResultado.text = "Melhor colocar gasolina!"
+                }
+            }
+        }
+        
+    }
+    
+    func validarCampo(precoAlcool: String, precoGasolina: String) -> Bool {
+        var camposValidados = true
+        if precoAlcool.isEmpty {
+            camposValidados = false
+        } else if precoGasolina.isEmpty {
+            camposValidados = false
+        }
+        return camposValidados
+    }
 
 }
 
